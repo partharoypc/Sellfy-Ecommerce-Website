d8bfa2d99ea63fb405fcc26095c74976df0c0ca6
Partha Roy <partharoypc@gmail.com>
2021-12-13 16:42:52 +0600

    Sellfy Website

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                "sellfy-button": "#00b140",
+                "sellfy-hover": "#00b131",
+                "sellfy-black": "#111",
+                "sellfy-gray": "#adadad",
+                "sellfy-gray2": "#666",
+                "sellfy-btn": "#DCA378",
+                "sellfy-bg-gray": "#F4F4F4",
+                "sellfy-btn-bg1": "#E5F7EC",
+                "sellfy-btn-bg2": "#3A95ED",
+                "sellfy-link-bg1": "#3c97ef",
+            },
+        },
+
+        fontFamily: {
+            Proxima: ["Proxima-nova,Helvetica Neue,Helvetica,Arial,sans-serif"],
+        },
+
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
